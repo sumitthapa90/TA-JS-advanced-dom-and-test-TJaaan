6fa8c30245b0e02abe044c25be8b376269d9aebc
sumit <sumitthapa90@gmail.com>
2021-07-26 15:54:25 +0530

    Practice

---
+++ block-BJaaiq/code/index.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+
+let limit = 3;
+let pageCount = 1;
+
+const getPost = async () => {
+  fetch(
+    `file:///home/sumit/Desktop/Advance%20Js/TA-JS-advanced-dom-and-test-TJaaan/block-BJaaiq/code/quotes.js`
+  );
+};
+getPost();
